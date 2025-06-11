0917129c05ddbf0e10f641009dee24b037667a2f
prajwal8074 <prajwal8074@gmail.com>
2025-06-11 19:11:14 +0530

    added Dialer build

---
+++ Call-Interface/DialerApp/app/build/generated/source/buildConfig/core/debug/com/simplemobiletools/dialer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.simplemobiletools.dialer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.simplemobiletools.dialer.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "core";
+  public static final int VERSION_CODE = 57;
+  public static final String VERSION_NAME = "5.18.1";
+}
