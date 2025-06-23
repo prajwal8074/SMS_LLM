b8eb6b186abfba83f20677dac5136e688719c883
adityasosa6040 <adityakumarsosa@gmail.com>
2025-06-23 16:48:29 +0530

    Delete MarketPlace directory

--- MarketPlace/marketplace-backend/server/database.py
+++
@@ -1,9 +0,0 @@
-import os
-import psycopg2
-from psycopg2 import sql
-import uuid # To generate UUIDs for new listings if not handled by DB automatically
-
-def get_db_connection():
-    """Establishes and returns a PostgreSQL database connection."""
-    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    return conn
