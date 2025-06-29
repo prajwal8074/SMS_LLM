41e24cd8dfe06488b725deab5e031f79ae726d02
prajwal8074 <prajwal8074@gmail.com>
2025-06-29 10:10:18 +0530

    Simple Test Case

--- AWS/Test/test_my_module.py
+++ AWS/Test/test_my_module.py
@@ -17,7 +17,7 @@ from my_module import add, subtract
 class TestMyModule(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 4)
+        self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
