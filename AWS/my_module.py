b7db9b5cc12f8975cd77975f92d6c1d9d2aa0fbf
prajwal8074 <prajwal8074@gmail.com>
2025-06-29 09:48:17 +0530

    Added simple unit test

---
+++ AWS/my_module.py
@@ -0,0 +1,13 @@
+# my_module.py
+
+def add(a, b):
+    """Adds two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Subtracts two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Multiplies two numbers."""
+    return a * b
