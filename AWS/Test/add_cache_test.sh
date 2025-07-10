776b7045fb895ee2662e85be6fcc154b621ac9e2
prajwal8074 <prajwal8074@gmail.com>
2025-07-11 01:33:44 +0530

    fix pre deployment workflow

--- AWS/Test/add_cache_test.sh
+++ AWS/Test/add_cache_test.sh
@@ -39,7 +39,7 @@ PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
 echo -e "\n--- Test adding temporary cache manually ---\n"
 echo "Testing for TTL: 60"
-key1=$(python "$PARENT_DIR"/add_cache.py "What is the capital of India?" "New Delhi" 60 | head -n 2 | tail -n 1)
+key1=$(python "$PARENT_DIR"/add_cache.py "What is the capital of India?" "New Delhi" 60 | head -n 3 | tail -n 1)
 echo -e "Extracted key for testing: '$key1'\n"
 echo "Checking TTL for key in Redis..."
 ttl_value=$(docker exec redis-test redis-cli TTL "$key1" | awk '{print $NF}' | tr -d '\r' | xargs)
@@ -52,7 +52,7 @@ else
 fi
 
 echo -e "\n--- Test adding permanent cache manually ---\n"
-key2=$(python "$PARENT_DIR"/add_cache.py "What is the capital of Canada?" "Ottawa" | head -n 2 | tail -n 1)
+key2=$(python "$PARENT_DIR"/add_cache.py "What is the capital of Canada?" "Ottawa" | head -n 3 | tail -n 1)
 echo -e "Extracted key for testing: '$key2'\n"
 echo "Checking TTL for key in Redis..."
 ttl_value=$(docker exec redis-test redis-cli TTL "$key2" | awk '{print $NF}' | tr -d '\r' | xargs)
